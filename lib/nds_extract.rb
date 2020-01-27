87220e2af2cd17e19af141b6cdfd2583a43d29f5
Ethan Davidson <theejdavidson@gmail.com>
2020-01-27 20:23:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,18 +11,18 @@ def directors_totals(nds)
       "0"
   }
   =begin
-  directors_index = 0
-  while directors_index < directors_database.length do
-    film_index = 0
-    director_total = 0
-    while film_index < directors_database[directors_index][:movies].length do
-      director_total += directors_database[directors_index][:movies][film_index][:worldwide_gross]
-      film_index += 1
-    end
-    result.store("#{directors_database[directors_index][:name]}",
-      "#{director_total}")
-    directors_index += 1
-  end
+  #directors_index = 0
+  #while directors_index < directors_database.length do
+   # film_index = 0
+    #director_total = 0
+    #while film_index < directors_database[directors_index][:movies].length do
+     # director_total += directors_database[directors_index][#:movies][film_index][:worldwide_gross]
+      #film_index += 1
+    #end
+    #result.store("#{directors_database[directors_index][:name]}",
+     # "#{director_total}")
+    #directors_index += 1
+#  end
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
