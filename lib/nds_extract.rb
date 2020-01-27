0863c623ffb0c04d9732e98affe0adec40e6fc86
Ethan Davidson <theejdavidson@gmail.com>
2020-01-27 20:29:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,9 +8,7 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   pp nds
-  result = {"#{nds[0][:name]}" =>
-      "0"
-  }
+  result = {"#{nds[0][:name]}" => "0"}
   #directors_index = 0
   #while directors_index < directors_database.length do
    # film_index = 0
